244c4d6dfae1ce5ac886a759f850c7ab291f4739
herkessal <mhnmhn1@hotmail.com>
2023-09-05 14:53:49 +0300

    dağıtım

---
+++ eXcel/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace eXcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.satir_button = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.ayarlarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.gelişmişSeçeneklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(14, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şirket İsimlerini Kısalt ve Tarihi düzelt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // satir_button
+            // 
+            this.satir_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.satir_button.Location = new System.Drawing.Point(-62, -49);
+            this.satir_button.Name = "satir_button";
+            this.satir_button.Size = new System.Drawing.Size(113, 44);
+            this.satir_button.TabIndex = 1;
+            this.satir_button.Text = "İlk Satırı ve kolonu sil";
+            this.satir_button.UseVisualStyleBackColor = true;
+            this.satir_button.Click += new System.EventHandler(this.satir_button_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(358, 70);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(133, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-> Maksimum Şirket isim uzunluğu ->";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 59);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "İŞLEM TÜRÜ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(136, 194);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(84, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            627,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(358, 128);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label2.Location = new System.Drawing.Point(188, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "-> Tarih Kaçıncı Sütunda ->";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(358, 98);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDown4.TabIndex = 8;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label3.Location = new System.Drawing.Point(158, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "-> Şirket İsmi Kaçıncı Sütunda ->";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 50);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "TEXT OLARAK KAYDET";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(489, 81);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 41);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "KDV2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(489, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 43);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "KDV1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.satir_button);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 405);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayarlarToolStripMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(599, 24);
+            this.menuStrip2.TabIndex = 14;
+            this.menuStrip2.Text = "menuStrip2";
+            this.menuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
+            // 
+            // ayarlarToolStripMenuItem1
+            // 
+            this.ayarlarToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gelişmişSeçeneklerToolStripMenuItem});
+            this.ayarlarToolStripMenuItem1.Name = "ayarlarToolStripMenuItem1";
+            this.ayarlarToolStripMenuItem1.Size = new System.Drawing.Size(54, 20);
+            this.ayarlarToolStripMenuItem1.Text = "ayarlar";
+            // 
+            // gelişmişSeçeneklerToolStripMenuItem
+            // 
+            this.gelişmişSeçeneklerToolStripMenuItem.CheckOnClick = true;
+            this.gelişmişSeçeneklerToolStripMenuItem.Name = "gelişmişSeçeneklerToolStripMenuItem";
+            this.gelişmişSeçeneklerToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.gelişmişSeçeneklerToolStripMenuItem.Text = "gelişmiş seçenekler";
+            this.gelişmişSeçeneklerToolStripMenuItem.Click += new System.EventHandler(this.gelişmişSeçeneklerToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(217, 22);
+            this.toolStripMenuItem1.Text = "Gelişmiş Seçenekleri Göster";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button satir_button;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem ayarlarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem gelişmişSeçeneklerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ eXcel/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+
+namespace eXcel
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string DosyaYolu = "";
+        string DosyaAdi = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "desktop";
+            ofd.Title = "Excel Dosyası Seçiniz...";
+            ofd.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            ofd.FilterIndex = 1;
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                 DosyaYolu = ofd.FileName;
+                 DosyaAdi = ofd.SafeFileName;
+                FileInfo fileInfo = new FileInfo(DosyaYolu);
+                ExcelPackage package = new ExcelPackage(fileInfo);
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+
+                
+
+
+                // kaç tane satır var say
+                int rows = worksheet.Dimension.Rows; 
+                
+               
+
+                
+
+                DialogResult dialogResult = MessageBox.Show("TARİH DÜZELTİLSİN Mİ?", "", MessageBoxButtons.YesNo);
+                int tarih = 0;
+                int sirket = 0;
+                if (numericUpDown3.Value!=0)
+                {
+                    tarih = Convert.ToInt32(numericUpDown3.Value);
+                }
+                if (numericUpDown4.Value!=0)
+                {
+                    sirket = Convert.ToInt32(numericUpDown4.Value);
+                }
+                
+                for ( int i = 1; i <= rows; i++) // satırlar arasında gez
+                {
+                    // ŞİRKET İSMİ KISALT
+                    if (worksheet.Cells[i, sirket].Value!=null && worksheet.Cells[i, sirket].Value.ToString().Length > 30&&numericUpDown4.Value!=0)
+                    {
+                        worksheet.Cells[i, sirket].Value = worksheet.Cells[i, sirket].Value.ToString().Substring(0, 30);
+                        if (i==rows)
+                        {
+                            MessageBox.Show("Şirket isimleri kısaltıldı.");
+                        }
+                    }
+                    
+                    
+                        
+                        if (dialogResult == DialogResult.Yes&&numericUpDown3.Value!=0)
+                        {
+                            if (worksheet.Cells[i, tarih].Value != null)
+                            {
+                                //TARİH '.' İŞARETİNDEN '/' İŞARETİNE DÖNÜŞTÜR. 13.01.2023 -> 13/01/2023
+                                worksheet.Cells[i, tarih].Value = worksheet.Cells[i, tarih].Value.ToString().Replace('.', '/');
+                                if (i == rows)
+                                {
+                                    MessageBox.Show("Tarih düzeltildi.");
+                                }
+                            }
+                        }
+                          
+                        
+                    
+                    
+                    
+
+                    
+                    
+                    
+                }
+                
+
+
+                // kaydet
+                package.Save();
+
+            }
+            
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            groupBox1.Visible = false;
+            button5.Dock = DockStyle.Fill;
+        }
+
+        private void satir_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "desktop";
+            ofd.Title = "Excel Dosyası Seçiniz...";
+            ofd.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = ofd.FileName;
+                DosyaAdi = ofd.SafeFileName;
+                FileInfo fileInfo = new FileInfo(DosyaYolu);
+                ExcelPackage package = new ExcelPackage(fileInfo);
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                worksheet.DeleteColumn(1); //birinci kolonu sil
+                MessageBox.Show("İlk kolon silindi.");
+                worksheet.DeleteRow(1); //birinci satırı sil
+                MessageBox.Show("İlk satır silindi.");
+                package.Save();
+            }
+                
+        }
+
+        private void kolon_button_Click(object sender, EventArgs e)
+        {
+            
+                
+               
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "desktop";
+            ofd.Title = "Excel Dosyası Seçiniz...";
+            ofd.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = ofd.FileName;
+                DosyaAdi = ofd.SafeFileName;
+                FileInfo fileInfo = new FileInfo(DosyaYolu);
+                ExcelPackage package = new ExcelPackage(fileInfo);
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+
+
+
+
+                // kaç tane satır var say
+                int rows = worksheet.Dimension.Rows;
+
+
+
+
+
+                
+
+                for (int i = 1; i <= rows; i++) // satırlar arasında gez
+                {
+                    worksheet.Cells[i, 7].Value = numericUpDown2.Value.ToString();
+                }
+                package.Save();
+            }
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Maximum = decimal.MaxValue;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {/*
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "desktop";
+            ofd.Title = "Excel Dosyası Seçiniz...";
+            ofd.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = ofd.FileName;
+                DosyaAdi = ofd.SafeFileName;
+                FileInfo fileInfo = new FileInfo(DosyaYolu);
+                ExcelPackage package = new ExcelPackage(fileInfo);
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                
+
+
+
+
+
+            }
+
+
+
+
+            */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); ;
+            ofd.Title = "Excel Dosyası Seçiniz...";
+            ofd.Filter = "Excel Dosyası |*.xlsx| Excel Dosyası|*.xls";
+            ofd.FilterIndex = 1;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = ofd.FileName;
+                DosyaAdi = ofd.SafeFileName;
+                FileInfo fileInfo = new FileInfo(DosyaYolu);
+                ExcelPackage package = new ExcelPackage(fileInfo);
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                worksheet.DeleteColumn(1); //birinci kolonu sil
+                MessageBox.Show("İlk kolon silindi.");
+                worksheet.DeleteRow(1); //birinci satırı sil
+                MessageBox.Show("İlk satır silindi.");
+                package.Save();
+            
+                
+
+
+
+                // kaç tane satır var say
+                int rows = worksheet.Dimension.Rows;
+
+
+
+
+
+                DialogResult dialogResult = MessageBox.Show("TARİH DÜZELTİLSİN Mİ?", "", MessageBoxButtons.YesNo);
+                int tarih = 0;
+                int sirket = 0;
+                if (numericUpDown3.Value != 0)
+                {
+                    tarih = Convert.ToInt32(numericUpDown3.Value);
+                }
+                if (numericUpDown4.Value != 0)
+                {
+                    sirket = Convert.ToInt32(numericUpDown4.Value);
+                }
+
+                for (int i = 1; i <= rows; i++) // satırlar arasında gez
+                {
+                    // ŞİRKET İSMİ KISALT
+                    if (worksheet.Cells[i, sirket].Value != null && worksheet.Cells[i, sirket].Value.ToString().Length > 30 && numericUpDown4.Value != 0)
+                    {
+                        worksheet.Cells[i, sirket].Value = worksheet.Cells[i, sirket].Value.ToString().Substring(0, 30);
+                        if (i == rows)
+                        {
+                            MessageBox.Show("Şirket isimleri kısaltıldı.");
+                        }
+                    }
+
+
+
+                    if (dialogResult == DialogResult.Yes && numericUpDown3.Value != 0)
+                    {
+                        if (worksheet.Cells[i, tarih].Value != null)
+                        {
+                            //TARİH '.' İŞARETİNDEN '/' İŞARETİNE DÖNÜŞTÜR. 13.01.2023 -> 13/01/2023
+                            worksheet.Cells[i, tarih].Value = worksheet.Cells[i, tarih].Value.ToString().Replace('.', '/');
+                            if (i == rows)
+                            {
+                                MessageBox.Show("Tarih düzeltildi.");
+                            }
+                        }
+                    }
+
+
+
+
+
+
+
+
+
+                }
+                for (int i = 1; i <= rows; i++) // satırlar arasında gez
+                {
+                    worksheet.Cells[i, 7].Value = numericUpDown2.Value.ToString();
+                }
+                package.Save();
+
+
+
+                // kaydet
+
+                
+                string kaydet = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                Microsoft.Office.Interop.Excel.Application xlApplication = new Microsoft.Office.Interop.Excel.Application();
+                xlApplication.Visible = true;
+                Microsoft.Office.Interop.Excel.Workbook workbook = xlApplication.Workbooks.Open(DosyaYolu);
+                workbook.Worksheets.Select(1);
+                //Microsoft.Office.Interop.Excel.Worksheet worksheet1 in workbook.Worksheets
+                
+                    //worksheet1.Activate();
+                    workbook.SaveAs(kaydet + "/KDV1.txt", Microsoft.Office.Interop.Excel.XlFileFormat.xlCurrentPlatformText);
+                
+
+
+
+            }
+
+
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+           
+        }
+
+        private void gelişmişSeçeneklerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (gelişmişSeçeneklerToolStripMenuItem.Checked==true)
+            {
+                groupBox1.Visible = true;
+                button5.Dock = DockStyle.None;
+            }
+            else
+            {
+                groupBox1.Visible = false;
+                button5.Dock = DockStyle.Fill;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
